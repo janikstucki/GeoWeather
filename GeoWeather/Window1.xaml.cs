4de9201b9d4cacbb50b0a8ced74eb401c5ae0043
janikstucki <janik.stucki@outlook.com>
2025-12-12 19:19:20 +0100

    Stations get now shown on the map

--- GeoWeather/Window1.xaml.cs
+++ GeoWeather/Window1.xaml.cs
@@ -1,19 +1,54 @@
 ﻿using System;
 using System.Windows;
 using Microsoft.Web.WebView2.Core;
+using Microsoft.Data.SqlClient;
+using System.Text.Json;
 
 namespace GeoWeather
 {
     public partial class Window1 : Window
     {
+        public string GetStations()
+        {
+            List<Station> stations = new List<Station>();
+
+            string query = "SELECT name, xCoordinate, yCoordinate FROM stations";
+            string connectionString = @"data source=PC-Janik\SQLEXPRESS;initial catalog=stations_db;trusted_connection=true;TrustServerCertificate=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            stations.Add(new Station
+                            {
+                                Name = reader.GetString(0),
+                                XCoordinate = reader.GetDouble(2),
+                                YCoordinate = reader.GetDouble(1)
+                            });
+                        }
+                    }
+                }
+            }
+            return JsonSerializer.Serialize(stations);
+        }
         public Window1()
         {
             InitializeComponent();
+
             Loaded += async (s, e) =>
             {
-                await Browser.EnsureCoreWebView2Async();
+                await Browser.EnsureCoreWebView2Async(); // kein Parameter!
                 string mapPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "map.html");
                 Browser.CoreWebView2.Navigate(new Uri(mapPath).AbsoluteUri);
+
+                // Stationen als JS-Array übergeben
+                string stationsJson = GetStations();
+                string js = $"window.stations = {stationsJson};"; // global verfügbar im HTML/JS
+                await Browser.CoreWebView2.ExecuteScriptAsync(js);
             };
         }
     }
